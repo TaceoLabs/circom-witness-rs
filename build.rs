a760cf956649b522fae5619180850b0b321328de
Fabian Gruber <fabian.gruber5@gmail.com>
2025-09-11 17:22:36 +0200

    feat: compile with --O2

--- build.rs
+++ build.rs
@@ -8,7 +8,9 @@ fn main() {
         let circuit_name = circuit_file.file_stem().unwrap().to_str().unwrap();
 
         let mut cmd = Command::new("circom");
-        cmd.arg(fs::canonicalize(circuit_file).unwrap()).arg("--c");
+        cmd.arg(fs::canonicalize(circuit_file).unwrap())
+            .arg("--c")
+            .arg("--O2");
         if library_path.is_ok() {
             cmd.arg("-l")
                 .arg(fs::canonicalize(library_path.unwrap()).unwrap());
